7fc0365c4200c3f66b3bb77c3dd43fdc7b3db041
shinfuru <torontoraptorsshin@gmail.com>
2022-09-26 23:52:05 +0900

    deploy

---
+++ app.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const app = express();
+const taskRoute = require("./routes/tasks");
+const connectDB = require("./db/connect");
+require("dotenv").config();
+
+app.use(express.json());
+app.use(express.static("./public"));
+
+const PORT = 5000;
+
+app.use("/api/v1/tasks", taskRoute);
+
+const start = async () => {
+  try {
+    await connectDB(process.env.MONGO_URL);
+    app.listen(PORT, console.log("サーバー起動"));
+  } catch (e) {
+    console.log(e);
+  }
+};
+
+start();
