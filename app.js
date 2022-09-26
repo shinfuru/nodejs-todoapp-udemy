57bf525cc92e2751afd77cde715b1786dfe48cc3
shinfuru <torontoraptorsshin@gmail.com>
2022-09-27 00:59:44 +0900

    mod dbURL

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ app.use("/api/v1/tasks", taskRoute);
 
 const start = async () => {
   try {
-    await connectDB(process.env.MONGO_URL);
+    await connectDB(process.env.MONGO_HEROKU_URL || process.env.MONGO_URL);
     app.listen(process.env.PORT || PORT, console.log("サーバー起動"));
   } catch (e) {
     console.log(e);
